1bf3ddfbcda94364628eff1b8607f745287b7ae0
dushibaiyu <dushibaiyu@yahoo.com>
2017-10-28 17:42:34 +0800

    idea

--- src/main/kotlin/me/duyu/yuedu/spider/HandleHome.kt
+++ src/main/kotlin/me/duyu/yuedu/spider/HandleHome.kt
@@ -12,7 +12,20 @@ fun hanleHomePage(data : String)
     //println("lefts size : ${lefts.size.toString()}")
     for (element in lefts){
         val tag = element.getElementById("columnimgleft").getElementsByTag("a").first();
-        println("tag tp string: ${tag.toString()}")
-       // println(tag.text());
+        saveUrl(tag.attr("abs:href"),tag.attr("title"))
+    }
+}
+
+fun saveUrl(url : String, title : String)
+{
+    println("url: $url")
+    println("title: $title")
+    if(url.startsWith("http://www.timetimetime.net/yuedu/"))
+    {
+
+    }
+
+    if(url.startsWith("http://www.timetimetime.net/yuedu/")){
+
     }
 }
